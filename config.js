5c6840bc9a6588e42a283f1bea094f8dba7bb7b8
DEXTER <dexterid2003@gmail.com>
2025-01-01 09:50:44 +0530

    Add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+module.exports = {
+    TELEGRAM_BOT_TOKEN: '8028709418:AAECk07-PLO2SbiNYeP5ayYNjxE3rqlRT9M', // Replace with your actual bot token
+    CHAT_ID: '7513039777', // Replace with your actual chat ID
+};
